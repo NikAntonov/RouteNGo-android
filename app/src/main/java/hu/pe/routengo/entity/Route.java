ca13e1ace0c05b2084e97d563200f23f1b1d72f4
CronSystems <cronhelp@gmail.com>
2017-02-18 14:30:40 +0200

    Initial commit

---
+++ app/src/main/java/hu/pe/routengo/entity/Route.java
@@ -0,0 +1,8 @@
+package hu.pe.routengo.entity;
+
+/**
+ * Created by Galya Sheremetova on 18.02.2017.
+ */
+
+public class Route {
+}
---
+++ app/src/main/java/hu/pe/routengo/model/RouteNGo.java
@@ -0,0 +1,8 @@
+package hu.pe.routengo.model;
+
+/**
+ * Created by Galya Sheremetova on 18.02.2017.
+ */
+
+public class RouteNGo {
+}
