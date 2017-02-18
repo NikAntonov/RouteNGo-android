e4f7b90ca358021992172b5c573052d34cadac87
snusmumriki <wdart1995@gmail.com>
2017-02-18 21:38:50 +0200

    i do not know

--- app/src/main/java/hu/pe/routengo/RouteNGoComponent.java
+++ app/src/main/java/hu/pe/routengo/RouteNGoComponent.java
@@ -1,8 +1,13 @@
 package hu.pe.routengo;
 
+import javax.inject.Singleton;
+
+import dagger.Component;
+
 /**
  * Created by Galya Sheremetova on 18.02.2017.
  */
-
+@Singleton
+@Component(modules = {ServiceModule.class, CacheModule.class})
 public class RouteNGoComponent {
 }
